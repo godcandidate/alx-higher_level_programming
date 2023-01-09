abff842448a484ec96ec670d7f70eca33c565a3e
godcandidate <godcandidate@gmail.com>
2023-01-09 21:44:28 +0000

    update 2-replace_in_list.py

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,17 +1,8 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    """My replace_in_list function
+    length = len(my_list) - 1
 
-    Args:
-        my_list: list to check
-        idx: index
-        element: element to replace
-
-    Returns:
-        Return value. return the original list if idx is out of
-        range or negative otherwise new list
-    """
-    if idx < 0 or idx > len(my_list):
+    if idx < 0 or idx > length:
         return (my_list)
 
     my_list[idx] = element
