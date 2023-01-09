1cec5796aee42a62a116e031ac54cf1e5364f74f
godcandidate <godcandidate@gmail.com>
2023-01-09 21:24:47 +0000

    update 1-element_at.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,15 +1,8 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    """My element_at function
+    length = len(my_list) - 1
 
-    Args:
-        my_list: list to check
-        idx: index
-
-    Returns:
-        Return value. return 0 if idx is out of
-        range or negative otherwise value of index
-    """
-    if idx < 0 or idx > len(my_list):
+    if idx < 0 or idx > length:
         return 0
+
     return (my_list[idx])
