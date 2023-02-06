246e4eb23f3047e4e33eb496cc1d1558244461ae
godcandidate <godcandidate@gmail.com>
2023-02-06 22:30:35 +0000

    update 0-lookup.py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def lookup(obj):
-    """ Function that returns the list of available attributes
+    """ A function that returns the list of available attributes
         and methods of an object
 
     Args:
