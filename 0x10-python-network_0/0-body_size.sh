b8abd32bca7578342656e7ee67a3a2a1c415a5c3
godcandidate <godcandidate@gmail.com>
2023-05-08 21:29:51 +0000

    update 0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # a bash script that  displays the size of the body of the response
-curl -sI 0.0.0.0:5000 | awk '/Content-Length/ {print $2}'
+curl -sI '$1' | awk '/Content-Length/ {print $2}'
