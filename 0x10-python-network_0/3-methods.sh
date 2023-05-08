afcda3ac17e7d05b22d49a08ad257802e0a1947d
godcandidate <godcandidate@gmail.com>
2023-05-08 22:38:02 +0000

    update 3-methods.sh

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# displays all HTTP methods the server will accept
-curl -sI "$1" | awk '/Allow/ {print $2, $3, $4}'
+# displays all HTTP methods
+curl -sI "$1" | grep "Allow" | cut -d " " -f 2-
