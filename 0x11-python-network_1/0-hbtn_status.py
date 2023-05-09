df708d6d4aa42fbf29fbe5d68ea4976f00152828
godcandidate <godcandidate@gmail.com>
2023-05-09 14:10:10 +0000

    update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,12 +2,12 @@
 #  a Python script that fetches https://alx-intranet.hbtn.io/status
 import urllib.request
 
-
 if __name__ == "__main__":
-    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status")
-    as response:
-        body = response.read()
-    print("Body response:")
-    print(f"\t- type: {type(body)}")
-    print(f"\t- content: {body}")
-    print(f"\t- utf8 content: {body.decode('utf-8')}")
+    requests = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+
+    with urllib.request.urlopen(requests) as response:
+        content = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode("utf-8")))
