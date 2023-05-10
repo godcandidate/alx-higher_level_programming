c4c448e3be20488e2d88846d92d5dfd5a7f7b761
godcandidate <godcandidate@gmail.com>
2023-05-10 18:03:02 +0000

    create 8-json_api.py

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""a Python script that takes in a letter and sends a POST"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        letter = {'q': ""}
+    else:
+        letter = {'q': sys.argv[1]}
+
+    url = "http://0.0.0.0:5000/search_user"
+    response = requests.post(url, data=letter)
+
+    try:
+        json_ = response.json()
+        if json_ == {}:
+            print("No result")
+        else:
+            print(f"[{json_.get('id')} {json_.get('name')}")
+
+    except ValueError:
+        print("Not a valid JSON")
