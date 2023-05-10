6683f03091c10bb6fb2840d2c2d3494a5a44e76a
godcandidate <godcandidate@gmail.com>
2023-05-10 15:43:09 +0000

    update 6-post_email.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -8,5 +8,5 @@ if __name__ == "__main__":
     url = sys.argv[1]
     email = {'email': sys.argv[2]}
 
-    html = requests.post(url, data = email)
-    print(html.text)
+    response = requests.post(url, data=email)
+    print(response.text)
