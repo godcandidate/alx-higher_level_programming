2a356ef7be13391999ed6443b63ff215cbcd3167
godcandidate <godcandidate@gmail.com>
2023-05-10 11:52:35 +0000

    update 4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,7 +4,7 @@ import requests
 
 
 if __name__ == "__main__":
-        body = requests.get("https://alx-intranet.hbtn.io/status")
-        print("Body response:")
-        print(f"\t- type: {type(body.text)}")
-        print(f"\t- content: {body.text}")
+    body = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:")
+    print(f"\t- type: {type(body.text)}")
+    print(f"\t- content: {body.text}")
