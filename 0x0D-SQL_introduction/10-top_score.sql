cb06db2389d63598f312d798932b0a213f3acf04
godcandidate <godcandidate@gmail.com>
2023-06-12 23:07:36 +0000

    update 10-top_score.sql

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,4 +1,4 @@
--- lists all rows of the table second_table
+-- this script lists all rows of the table second_table
 -- from the database hbtn_0c_0
 -- results display score and name
 -- ordered by score
