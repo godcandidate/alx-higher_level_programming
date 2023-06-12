52e84f98f7ea1783c43cb2cbdbcffae8afe442b1
godcandidate <godcandidate@gmail.com>
2023-06-12 23:07:37 +0000

    update 15-groups.sql

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,6 +1,5 @@
--- Lists the number of records with the same score
--- in the table `second_table` of the database `hbtn_0c_0`
--- in MySQL Server.
+-- lists the number of records with the same score
+-- from `second_table` of the database `hbtn_0c_0`
 
 SELECT score, COUNT('score') as number
 FROM second_table
