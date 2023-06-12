a809fa180aa8ba3f7d0c6ae5141227df64e5964d
godcandidate <godcandidate@gmail.com>
2023-06-12 23:07:37 +0000

    update 7-insert_value.sql

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,4 +1,4 @@
--- inserts a new row in table first_table
+-- inserts a new record in table first_table
 -- in database hbtn_0c_0
 INSERT INTO first_table(id, name) VALUES (89, 'Best School');
 Give feedback
